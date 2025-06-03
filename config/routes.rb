680466c96f1fec47033a32049f92053f06707931
Sam Adesoga <samueladesoga@gmail.com>
2025-06-03 20:19:25 +0100

    added scaffold for create rooming

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :rooms
   root to: "home#index"
   resource :session
   resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_03_155231) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_03_191054) do
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
