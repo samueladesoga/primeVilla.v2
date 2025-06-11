f87f61820ec952ff1040b3ef7436f94a19fcd6e0
Sam Adesoga <samueladesoga@gmail.com>
2025-06-11 23:25:49 +0100

    added mailers to code

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,14 +7,16 @@ class RegistrationsController < ApplicationController
     end
 
     def create
-        @user = User.new(user_params)
-        if @user.save
-          redirect_to root_path, notice: "You have created a new Tenant successfully."
-        else
-          flash[:alert] = @user.errors.full_messages.join(", ")
-          render :new, status: :unprocessable_entity
-        end
+      generated_password = SecureRandom.alphanumeric(12)
+      @user = User.new(user_params.merge(password: generated_password, password_confirmation: generated_password))
+      if @user.save
+        UserMailer.with(user: @user, password: generated_password).welcome_email.deliver_later
+        redirect_to root_path, notice: "You have created a new Tenant successfully. The password has been sent to their email."
+      else
+        flash[:alert] = @user.errors.full_messages.join(", ")
+        render :new, status: :unprocessable_entity
       end
+    end
     
     private
     
