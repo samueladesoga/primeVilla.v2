6be2991a3175f7f2cbd9379d85d899636506e193
Sam Adesoga <samueladesoga@gmail.com>
2025-06-22 20:18:37 +0100

    reinserted date picker

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,6 +2,8 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+import DatePickerController from "./date_picker_controller"
+application.register("date-picker", DatePickerController)
 
 
 
