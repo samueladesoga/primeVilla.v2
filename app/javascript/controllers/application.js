9a0d861a8e2be58ec8d12b27f70a91559eaed5c5
Sam Adesoga <samueladesoga@gmail.com>
2025-06-22 21:29:47 +0100

    fixed

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,8 @@
 import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-importmap-autoloader"
 
 const application = Application.start()
+application.load(definitionsFromContext("controllers"))
 
 // Configure Stimulus development experience
 application.debug = false
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,7 +2,7 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
-import DatePickerController from "./date_picker_controller"
+import DatePickerController from "./date_picker_controller.js"
 application.register("date-picker", DatePickerController)
 
 
