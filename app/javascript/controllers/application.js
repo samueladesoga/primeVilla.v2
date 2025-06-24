caefd4af1a3815730c1f2e7ebbc1e806d7e673f9
Sam Adesoga <samueladesoga@gmail.com>
2025-06-24 06:31:10 +0100

    finally for the date picker work and auto registration os timulus controllers

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,8 +1,6 @@
 import { Application } from "@hotwired/stimulus"
-import DatePickerController from "./date_picker_controller"
 
 const application = Application.start()
-application.register("date-picker", DatePickerController)
 application.debug = false
 window.Stimulus = application
 
