aa36e28365704dd9874bc231d6e85b3a74c9bb77
Sam Adesoga <samueladesoga@gmail.com>
2025-07-07 16:32:34 +0100

    fixed background job

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,6 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-  def show
-    @user = User.find(params[:id])
-  end
 
   def edit
     @user = User.find(params[:id])
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,20 +10,20 @@ class UserMailer < ApplicationMailer
     mail(to: @tenancy.user.email_address, subject: "Tenancy Created on SS Hall Hostel Management System")
   end
 
-  	def rent_due_soon(user_id, tenancy_end_date)
-	  	user = User.find(user_id)
-	    @email = user.email
-		  @tenancy_end_date = tenancy_end_date
-	    @full_name = user.full_name
+  	def rent_due_soon
+	  	user = User.find(params[:user_id])
+	    @email = user.email_address
+      @full_name = user.name
+		  @tenancy_end_date = params[:tenancy_end_date].strftime('%a, %e %b %Y')
 	    mail(to: @email, bcc: "samueladesoga@gmail.com", subject: 'SS Hall: Your rent is due soon')
 	end
 
-	def rent_over_due(user_id, tenancy_end_date, elapsed_days)
-	  	user = User.find(user_id)
-	    @email = user.email
-	    @full_name = user.full_name
-	    @tenancy_end_date = tenancy_end_date
-	    @elapsed_days = elapsed_days
+	def rent_over_due
+	  	user = User.find(params[:user_id])
+	    @email = user.email_address
+	    @full_name = user.name
+	    @tenancy_end_date = params[:tenancy_end_date].strftime('%a, %e %b %Y')
+	    @elapsed_days = params[:elapsed_days]
 	    mail(to: @email, bcc: "samueladesoga@gmail.com", subject: 'SS Hall: Your rent is over due')
 	end
 end
