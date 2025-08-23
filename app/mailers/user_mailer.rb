60b15bad24ef216a213904218d212ddf94d1be7d
Sam Adesoga <samueladesoga@gmail.com>
2025-08-23 21:06:01 +0100

    cc in all email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,12 +2,12 @@ class UserMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
     @password = params[:password]
-    mail(to: @user.email_address, subject: "Welcome to the SSHall hostel management system")
+    mail(to: @user.email_address, bcc: "samueladesoga@gmail.com", subject: "Welcome to the SSHall hostel management system")
   end
 
   def tenancy_created
     @tenancy = params[:tenancy]
-    mail(to: @tenancy.user.email_address, subject: "Tenancy Created on SS Hall Hostel Management System")
+    mail(to: @tenancy.user.email_address, bcc: "samueladesoga@gmail.com", subject: "Tenancy Created on SS Hall Hostel Management System")
   end
 
   	def rent_due_soon
